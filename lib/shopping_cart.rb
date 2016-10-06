ef982e519b96c0cc24491a7166ad967cf41f0873
Daniel Bush <dlb.id.au@gmail.com>
2016-10-06 22:44:15 +1100

    FEATURE: we can add items to the cart.

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,2 +1,15 @@
 class ShoppingCart
+
+  def initialize
+    @products = []
+  end
+
+  def add product
+    @products.push(product)
+  end
+
+  def total
+    @products.inject(0){|sum, product| sum += product.price }
+  end
+
 end
--- spec/cart_spec.rb
+++ spec/cart_spec.rb
@@ -1,7 +1,19 @@
 require 'shopping_cart'
+require 'pp'
 
 describe ShoppingCart do
+
   it "can be instantiated with no pricing rules" do
     expect{ ShoppingCart.new }.not_to raise_error
   end
+
+  context ShoppingCart, "#add" do
+    it "should increase the cart total" do
+      product = double('Product', { code: 'ult_small', name: 'Unlimited 1G', price: 10 })
+      cart = ShoppingCart.new
+      cart.add(product)
+      expect(cart.total).to eq(10)
+    end
+  end
+
 end
