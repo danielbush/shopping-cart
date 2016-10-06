7673e82338bc608009353c6c49ee81664bb746e8
Daniel Bush <dlb.id.au@gmail.com>
2016-10-06 22:51:21 +1100

    FEATURE: ensure cart initial total is 0.

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -7,6 +7,10 @@ describe ShoppingCart do
     expect{ ShoppingCart.new }.not_to raise_error
   end
 
+  it "should initialise with a total of 0" do
+    expect(ShoppingCart.new.total).to eq 0
+  end
+
   context ShoppingCart, "#add" do
     it "should increase the cart total" do
       product = double('Product', { code: 'ult_small', name: 'Unlimited 1G', price: 10 })
