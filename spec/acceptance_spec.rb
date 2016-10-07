1406c1e94a66f8eb3de09dcf34ed173f5d1a439e
Daniel Bush <dlb.id.au@gmail.com>
2016-10-08 10:51:18 +1100

    SETUP: moving real examples into shared examples.

--- spec/acceptance_spec.rb
+++ spec/acceptance_spec.rb
@@ -2,22 +2,11 @@
 
 require 'shopping_cart'
 require 'pricing_rules_factory'
-
+require 'examples'
 
 describe "shopping cart scenarios" do
 
-  let(:ult_small) {
-    double('Product', { code: 'ult_small', name: 'Unlimited 1G', price: 24.90 })
-  }
-  let(:ult_medium) {
-    double('Product', { code: 'ult_medium', name: 'Unlimited 2G', price: 29.90 })
-  }
-  let(:ult_large) {
-    double('Product', { code: 'ult_large', name: 'Unlimited 5G', price: 44.90 })
-  }
-  let(:datapack_1G) {
-    double('Product', { code: '1gb', name: '1 GB Data-pack', price: 9.90 })
-  }
+  include_context 'examples'
 
   # High-level sketch...
 
--- spec/examples.rb
+++ spec/examples.rb
@@ -1,4 +1,20 @@
 
 RSpec.shared_context "examples", :shared_context => :metadata do
-  let(:shared_let) { {'arbitrary' => 'object'} }
+
+  let(:ult_small) {
+    double('Product', { code: 'ult_small', name: 'Unlimited 1G', price: 24.90 })
+  }
+
+  let(:ult_medium) {
+    double('Product', { code: 'ult_medium', name: 'Unlimited 2G', price: 29.90 })
+  }
+
+  let(:ult_large) {
+    double('Product', { code: 'ult_large', name: 'Unlimited 5G', price: 44.90 })
+  }
+
+  let(:datapack_1G) {
+    double('Product', { code: '1gb', name: '1 GB Data-pack', price: 9.90 })
+  }
+
 end
