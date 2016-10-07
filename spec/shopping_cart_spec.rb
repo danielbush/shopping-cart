8104a1c70d3a9c6f0323ada4c203b74820248e4e
Daniel Bush <dlb.id.au@gmail.com>
2016-10-07 21:59:11 +1100

    FEATURE: ShoppingCart can list items.

--- spec/shopping_cart_spec.rb
+++ spec/shopping_cart_spec.rb
@@ -6,6 +6,9 @@ describe ShoppingCart do
   let(:product1) {
     double('Product', { code: 'prod1-code', name: 'prod1-name', price: 10 })
   }
+  let(:product2) {
+    double('Product', { code: 'prod2-code', name: 'prod2-name', price: 15 })
+  }
 
   it "can be instantiated with no pricing rules" do
     expect { ShoppingCart.new }.not_to raise_error
@@ -28,6 +31,17 @@ describe ShoppingCart do
     it "for empty cart, should return empty array" do
       cart = ShoppingCart.new
       expect(cart.items).to eq []
+    end
+
+    it "should display a line for each different item" do
+      cart = ShoppingCart.new
+      cart.add(product1)
+      cart.add(product2)
+      line1 = { code: 'prod1-code', description: 'prod1-name',
+                count: 1, price: 10, cost: 10 }
+      line2 = { code: 'prod2-code', description: 'prod2-name',
+                count: 1, price: 15, cost: 15 }
+      expect(cart.items).to eq([ line1, line2 ])
     end
 
     it "should aggregate products with same code" do
